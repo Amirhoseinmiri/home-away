05a8d5508d389f4ab163ed488f1d5b0513931e69
Amir <A.Miri>
2024-09-23 11:50:49 +0330

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="destructive" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
+}
