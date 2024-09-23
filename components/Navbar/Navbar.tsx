3f16b834c312f1299090d4cf03bc3d2c00d66647
Amir <A.Miri>
2024-09-23 14:59:39 +0330

    add clerk

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import NavSearch from "./NavSearch";
+import Logo from "./Logo";
+import DarkMode from "./DarkMode";
+import LinkDropDown from "./LinkDropDown";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="border-b ">
+      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <DarkMode />
+          <LinkDropDown />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
