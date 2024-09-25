4bcb37bd3847390b78b758b1556cd65deb1b0636
Amir <A.Miri>
2024-09-25 18:00:51 +0330

    complete fetching data and create property

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-import { Button } from "@/components/ui/button";
+import LoadingCards from "@/components/card/LoadingCards";
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
+import { Suspense } from "react";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
+  console.log(searchParams);
   return (
-    <div>
-      <h1 className="text-3xl">HomePage</h1>
-      <Button variant="destructive" size="lg" className="capitalize m-8">
-        Click me
-      </Button>
-    </div>
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams.category}
+          search={searchParams.search}
+        />
+      </Suspense>
+    </section>
   );
 }
