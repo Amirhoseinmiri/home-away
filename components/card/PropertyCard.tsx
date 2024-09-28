51065d266c89cc317715f36539caa9c65e48c563
Amir <A.Miri>
2024-09-28 09:56:36 +0330

    deploy

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -29,7 +29,7 @@ const PropertyCard = ({ property }: { property: PropertyCardProps }) => {
             {name.substring(0, 30)}
           </h3>
           {/* property rating */}
-          <PropertyRaiting inPage={false} propertyId={propertyId} />
+          <PropertyRaiting inPage={false} />
         </div>
         <p className="text-sm mt-1 text-muted-foreground ">
           {tagline.substring(0, 40)}
