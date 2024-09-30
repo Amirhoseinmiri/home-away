d5e71c2d57f46c508ce3c6ca08390856aa57e59d
Amir <A.Miri>
2024-09-30 17:42:19 +0330

    create admin route

---
+++ app/reservations/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+import LoadingTable from "@/components/booking/LoadingTabel";
+import React from "react";
+
+const Loading = () => {
+  return <LoadingTable />;
+};
+
+export default Loading;
--- components/form/AmenitiesInput.tsx
+++ components/form/AmenitiesInput.tsx
@@ -4,8 +4,13 @@ import { amenities, Amenity } from "@/utils/amenities";
 import { Checkbox } from "@/components/ui/checkbox";
 
 const AmenitiesInput = ({ defaultValue }: { defaultValue?: Amenity[] }) => {
+  const amenitiesWithIcons = defaultValue?.map(({ name, selected }) => ({
+    name,
+    selected,
+    icon: amenities.find((amenity) => amenity.name === name)!.icon,
+  }));
   const [selectedAmenities, setSelectedAmenities] = useState<Amenity[]>(
-    defaultValue || amenities
+    amenitiesWithIcons || amenities
   );
 
   const handleChange = (amenity: Amenity) => {
@@ -38,7 +43,7 @@ const AmenitiesInput = ({ defaultValue }: { defaultValue?: Amenity[] }) => {
               className="text-sm font-medium leading-none capitalize flex gap-x-2 items-center"
             >
               {amenity.name}
-              <amenity.icon className="w-4 h-4" />
+              {amenity.icon && <amenity.icon className="w-4 h-4" />}
             </label>
           </div>
         ))}
